bee3bad811fae02d33c3a7c1dcb6b1fdfb0a2402
jamie956 <tgwzzjm@gmail.com>
2020-02-11 19:09:41 +0800

    [Update] countAndSay

--- countAndSay.js
+++ countAndSay.js
@@ -1,35 +1,32 @@
-function generateCountAndChar(str) {
-  let count = 0;
-  let current = str.charAt(0);
-  let rs = '';
-  for(let i=0; i<str.length; i++){
-    if(current == str.charAt(i)){
-      count++;
-    }else{
-      rs = rs + count + current;
-      count = 1;
-      current = str.charAt(i);
-    }
-  }
-  rs = rs + count + current;
-  return rs;
-}
+/*
+38. Count and Say
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+
+*/
 
 /**
  * @param {number} n
  * @return {string}
  */
-var countAndSay = function(n) {
-  let str = '1';
-  let i = 1;
-  if(n == 1){
-    return str;
-  }
-  while(i < n){
-    str = generateCountAndChar(str);
-    i++;
-  }
-  return str;
+var countAndSay = function (n) {
+  return recursion('1', 1, n);
 };
+
+function recursion(str, cur, n) {
+  if (cur == n) return str;
+  let count = 0;
+  let prev = str.charAt(0);
+  let ans = '';
+  for (let i = 0; i <= str.length; i++) {
+    if (prev == str.charAt(i)) {
+      count++;
+    } else {
+      ans = ans + count + prev;
+      count = 1;
+      prev = str.charAt(i);
+    }
+  }
+  return recursion(ans, cur + 1, n);
+}
 
 console.log(countAndSay(5));
