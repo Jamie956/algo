c0e29dca7095b6f4d7ea9266fcb527719aac004b
jamie956 <tgwzzjm@gmail.com>
2020-02-11 12:11:09 +0800

    Update climbStairs

--- climbStairs.js
+++ climbStairs.js
@@ -1,15 +1,18 @@
+/*
+70. Climbing Stairs
+You are climbing a stair case. It takes n steps to reach to the top.
 
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
 
 /**
  * @param {number} n
  * @return {number}
  */
 var climbStairs = function (n) {
-  if (n == 1) return 1;
-  let dp = [];
-  dp[1] = 1;
-  dp[2] = 2;
-  for (let i = 3; i <= n; i++) {
+  if (n < 4) return n;
+  let dp = [undefined, 1, 2, 3];
+  for (let i = 4; i <= n; i++) {
     dp[i] = dp[i - 1] + dp[i - 2];
   }
   return dp[n];
@@ -18,6 +21,15 @@ var climbStairs = function (n) {
 let ans = climbStairs(4);
 console.log(ans);
 /*
-1 2 3 4 5 6 7
-1 2 3 5 8
+爬1级楼梯，有1种方式，1
+爬2级楼梯，有2种方式，1+1、2
+爬3级楼梯，有3种方式，1+1+1、2+1、1+2
+爬4级楼梯，有5种方式，1+1+1+1、2+2、2+1+1、1+2+1、1+1+2
+爬5级楼梯，有8种方式，1+1+1+1+1、2+2+1、1+2+2、2+1+2、2+1+1+1、1+2+1+1、1+1+2+1、1+1+1+2
+
+设n为阶级数，N为方式数，得到f(n)=N
+n=1, f(n)=N=1
+n=2, f(n)=N=2
+n=3, f(n)=N=3
+n>3, f(n)=N=f(n-1)+f(n-2)
 */
