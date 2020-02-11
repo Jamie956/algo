bc947ebddf786c3d2eadd55479ae0bed69e4ea2a
jamie956 <tgwzzjm@gmail.com>
2020-02-11 22:13:19 +0800

    [Update] dailyTemperatures

--- dailyTemperatures.js
+++ dailyTemperatures.js
@@ -1,34 +1,31 @@
 /*
-new result array
-new stack
-for each from end to begin
-  while(stack not empty and current T[i] >= T(stack peek))
-    stack pop, pop out all element that less than T[i], till find the element more than T[i]
-  in this step, all element that less than T[i] was removed from stack befored find to T[i] < stack peek
-  result[i] = stack empty ? 0 : peek - i
-  stack push i
+739. Daily Temperatures
+Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
 */
 
 /**
  * @param {number[]} T
  * @return {number[]}
  */
-var dailyTemperatures = function(T) {
-  let result = [];
+var dailyTemperatures = function (T) {
   let stack = [];
-  for (let i = T.length - 1; i >= 0; --i) {
-    while(stack.length>0 && T[i]>=T[stack[stack.length-1]]){
+  let ans = [];
+  for (let i = T.length - 1; i >= 0; i--) {
+    while (T[i] >= T[stack[stack.length - 1]]) {
       stack.pop();
     }
-    result[i] = (stack.length == 0) ? 0 : (stack[stack.length-1] - i);
+    ans[i] = (stack.length == 0) ? 0 : (stack[stack.length - 1] - i);
     stack.push(i);
   }
-  return result;
+  return ans;
 };
 
-let T;
-T = [73, 74, 75, 71, 69, 72, 76, 73];//[1, 1, 4, 2, 1, 1, 0, 0]
-T = [89,62,70,58,47,47,46,76,100,70];//[8,1,5,4,3,2,1,1,0,0]
+/*
+从最后一位开始遍历数组T
+如果当前T[i]>= T[j]（顶栈元素j），那么弹出顶栈元素，直到找到T[i]<T[j]
+如果栈为空，说明不存在比T[i]大的温度，那么ans[i]=0
+如果栈有值，距离升温的天数就 i与顶栈元素之差的绝对值，and[i]=|i-j|
+i入栈
+*/
 
-let result = dailyTemperatures(T);
-console.log(result);
+console.log(dailyTemperatures([89, 62, 70, 58, 47, 47, 46, 76, 100, 70]));//[8,1,5,4,3,2,1,1,0,0]
