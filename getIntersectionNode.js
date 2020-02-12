f6990a6e141527e7bd9824f2a6bf8ccdaccdf4b9
jamie956 <tgwzzjm@gmail.com>
2020-02-12 15:03:03 +0800

    [Update] getIntersectionNode

--- getIntersectionNode.js
+++ getIntersectionNode.js
@@ -1,12 +1,8 @@
+/*
+160. Intersection of Two Linked Lists
+Write a program to find the node at which the intersection of two singly linked lists begins.
+*/
 
-
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
 function ListNode(val) {
   this.val = val;
   this.next = null;
@@ -18,34 +14,40 @@ function ListNode(val) {
  */
 var getIntersectionNode = function (headA, headB) {
   if (headA == null || headB == null) return null;
-  let ha = headA;
-  let hb = headB;
-  while (ha != hb) {
-    ha = (ha == null) ? headB : ha.next;
-    hb = (hb == null) ? headA : hb.next;
+  let curA = headA;
+  let curB = headB;
+  while (curA != curB) {
+    curA = curA == null ? headB : curA.next;
+    curB = curB == null ? headA : curB.next;
   }
-  return ha;
-};
+  return curA;
+}
+
+
 //1
-// let share = new ListNode(2);
-// share.next = new ListNode(4);
+let public = new ListNode(2);
+public.next = new ListNode(4);
 
-// let headA = new ListNode(0);
-// headA.next = new ListNode(9);
-// headA.next.next = new ListNode(1);
-// headA.next.next.next = share;
+let headA = new ListNode(0);
+headA.next = new ListNode(9);
+headA.next.next = new ListNode(1);
+headA.next.next.next = public;
 
-// let headB = new ListNode(3);
-// headB.next = share;
+let headB = new ListNode(3);
+headB.next = public;
 
 //2
-let headA = new ListNode(2);
-headA.next = new ListNode(6);
-headA.next.next = new ListNode(4);
+// let headA = new ListNode(2);
+// headA.next = new ListNode(6);
+// headA.next.next = new ListNode(4);
 
-let headB = new ListNode(1);
-headB.next = new ListNode(5);
+// let headB = new ListNode(1);
+// headB.next = new ListNode(5);
 
-let ans = getIntersectionNode(headA, headB);
-console.log(ans);
+console.log(getIntersectionNode(headA, headB));
 
+/*
+找两条单链共同的节点，不仅是节点的值相等，而且是同一个对象
+AB链各一个指针，假设A链较短，会先满足node==null，说明指针已经到这条链的末尾，这时将指针指向B链的head
+当B链的指针走到末尾即node==null，使指针指向A链，这样做可以使两个指针到尾部的距离都是相等的，意味着指针会相遇（如果两条链有共同节点）
+*/
