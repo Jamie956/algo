2d637c00868bb7e37fb47d746b9f14d6c811c0e1
jamie956 <tgwzzjm@gmail.com>
2020-02-12 10:33:51 +0800

    [Update] diameterOfBinaryTree

--- diameterOfBinaryTree.js
+++ diameterOfBinaryTree.js
@@ -1,10 +1,14 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+/*
+543. Diameter of Binary Tree
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+*/
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
 /**
  * @param {TreeNode} root
  * @return {number}
@@ -23,3 +27,20 @@ function depth(root) {
   diameter = Math.max(leftDepth + rightDepth, diameter);
   return Math.max(leftDepth, rightDepth) + 1;
 }
+
+let root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+console.log(diameterOfBinaryTree(root));
+
+/*
+树考虑递归
+从左下角开始往上计算
+终止条件node==null
+要有一个变量存diameter，diameter与左右节点之和比较
+递归参数node.left, node.right
+返回值，当前root节点的左右节点中，最长的左或右节点
+*/
