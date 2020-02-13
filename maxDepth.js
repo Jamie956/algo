6825b734c94647b9729de340f3af67dbd94c8544
jamie956 <tgwzzjm@gmail.com>
2020-02-13 20:37:56 +0800

    [Update] maxDepth

--- maxDepth.js
+++ maxDepth.js
@@ -1,23 +1,20 @@
 /*
-rightDepth(node)
-  if node == null, return 0
-  leftDepth = recursion(node.left)
-  rightDepth = recursion(node.right)
-  maxDepth = max(leftDepth, rightDepth)+1
-  return maxDepth
+104. Maximum Depth of Binary Tree
+
+Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+由root开始到枝末，求最大深度
+树结构用递归
+终止条件 node==null
+返回深度
+实质上求左右节点中深度较大的一个，max(left, right)
 */
 
 function TreeNode(val) {
   this.val = val;
   this.left = this.right = null;
 }
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
 /**
  * @param {TreeNode} root
  * @return {number}
@@ -26,60 +23,23 @@ var maxDepth = function (root) {
   if (root == null) return 0;
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
-/*
-maxDepth(1) => 4
-
-maxDepth(2) => 3
-
-maxDepth(3)
-maxDepth(null)
-maxDepth(null) => 1
-maxDepth(4) => 2
-maxDepth(5)
-maxDepth(null)
-maxDepth(null) => 1
-maxDepth(6)
-maxDepth(null)
-maxDepth(null) => 1
-
-maxDepth(7)
-maxDepth(null)
-maxDepth(null) => 1
-
-*/
 
 let root = new TreeNode(1);
-let node11 = new TreeNode(2);
-let node12 = new TreeNode(7);
-let node21 = new TreeNode(3);
-let node22 = new TreeNode(4);
-let node31 = new TreeNode(5);
-let node32 = new TreeNode(6);
-root.left = node11;
-root.right = node12;
-node11.left = node21;
-node11.right = node22;
-node22.left = node31;
-node22.right = node32;
+root.left = new TreeNode(2);
+root.right = new TreeNode(7);
+root.left.left = new TreeNode(3);
+root.left.right = new TreeNode(4);
+root.left.right.left = new TreeNode(5);
+root.left.right.right = new TreeNode(6);
 
-// let ans = maxDepth(root);
-// console.log(ans);
+console.log(maxDepth(root));
 
 /*
-root not null,if root == null, return 0
-defined stack, depth
-push root to stack
-while stack not empty{
-  stack.size
-  depth++
-  for, length is size
-    stack pop node
-    if pop node.left != null
-      node.left push to stack
-    if pop node.right != null
-      node.right push to stack
-}
-return depth
+用栈解深度
+初始root入栈
+深度+1
+判断当前层节点是否有下一个节点
+同一层的节点全部入栈
 */
 function maxDepth2(root) {
   if (root == null) return 0;
@@ -92,16 +52,11 @@ function maxDepth2(root) {
     depth++;
     for (let i = 0; i < size; i++) {
       let node = stack.shift();
-      if (node.left != null) {
-        stack.push(node.left);
-      }
-      if (node.right != null) {
-        stack.push(node.right);
-      }
+      if (node.left != null) stack.push(node.left);
+      if (node.right != null) stack.push(node.right);
     }
   }
   return depth;
 }
 
-let ans = maxDepth2(root);
-console.log(ans);
+console.log(maxDepth2(root));
