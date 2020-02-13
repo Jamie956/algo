355cf4c9cb5fd709ead87e8c90302c3574c14e79
jamie956 <tgwzzjm@gmail.com>
2020-02-13 21:23:33 +0800

    [Update] maxProfit

--- maxProfit.js
+++ maxProfit.js
@@ -1,25 +1,18 @@
 /*
-Say you have an array for which the ith element is the price of a given stock on day i.
+121. Best Time to Buy and Sell Stock
 
+Say you have an array for which the i element is the price of a given stock on day i.
 If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
-
 Note that you cannot sell a stock before you buy one.
 
-Input: [7,1,5,3,6,4]
-Output: 5
-
-for s1 in all status1
-  for s2 in all status2
-    for ...
-      dp[s1][s2][...] = best choose(selet1, select2, ...)
-
-
-selection: buy, sell, rest(do nothing)
+每天有3种动作：买、卖、什么都不做
 
 dp[i][k][s]
 i 天数
 k 交易次数
-s status, has stock=1, no stock=0
+s 状态, 手持股票=1, 还没买股票=0
+
+罗列所有可能的情况，寻找最大值
 
 for 0 <= i < n
   for 1 <= k < K
@@ -27,15 +20,17 @@ for 0 <= i < n
       dp[i][k][s] = max(buy, sell, rest)
 
 base case
+没买股票，一定是没有收支
 dp[i][0][0]=0
-dp[i][0][1]=null
-
 dp[-1][k][0]=0
+收上有股票时，交易次数不为0
+dp[i][0][1]=null
+天数不存在负数
 dp[-1][k][1]=null
 
 formula
-dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i]), max(rest, sell)
-dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i]), max(rest, buy)
+dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i]) //max(rest, sell)
+dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i]) //max(rest, buy)
 
 */
 
@@ -44,26 +39,16 @@ dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i]), max(rest, buy)
  * @return {number}
  */
 var maxProfit = function(prices) {
-  if(prices.length == 0){
-    return 0;
-  }
-  //i, s
-  //dp[0][1]=prices[0]
-  //dp[0][0]=0
+  if(prices.length == 0) return 0;
+  
   let dp = [];
   dp[0] = [0, -prices[0]];
 
   for (let i = 1; i < prices.length; i++) {
-    //s=0
-    // dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]+prices[i]);
-    //s=1
-    // dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
-
     dp[i] = [Math.max(dp[i-1][0], dp[i-1][1]+prices[i]), Math.max(dp[i-1][1], -prices[i])];
   }
 
   return dp[prices.length-1][0];
 };
 
-let a = maxProfit([7,1,5,3,6,4]);
-console.log(a);
+console.log(maxProfit([7,1,5,3,6,4]));
