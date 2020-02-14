e0a525404bca3ffa5d48374e0578b8d11872fcf0
jamie956 <tgwzzjm@gmail.com>
2020-02-14 21:13:22 +0800

    [Update] maxSubArray

--- maxSubArray.js
+++ maxSubArray.js
@@ -1,60 +1,25 @@
 /*
-Given an integer array nums,
-find the contiguous subarray (containing at least one number)
-which has the largest sum and return its sum.
+53. Maximum Subarray
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
-Input: [-2,1,-3,4,-1,2,1,-5,4],
-Output: 6
-Explanation: [4,-1,2,1] has the largest sum = 6.
-*/
+给出一个整数数组，找出这样一个连续的子数组，它的各个元素和是最大的，求它的和
 
-/*
-  define max_sum_before_ending, 和的最大值，其中必须包括最后一个元素
-  define max, answer
-  foreach nums
-    max_sum_before_ending = max(num[i], max_sum_before_ending+num[i]) //sum[i] = max(a[i], sum[i-1]+a[i])
-    max = max(max, max_sum_before_ending)
-  return max
+计算一个连续子数组的和，如果当前元素加上他们的和反而变小了，那就让当前元素算作下一个连续数组
+在这些之中找出最大值
 */
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-  if(!nums.length){
-    return;
-  }
-  let max_sum_before_ending = nums[0];
+var maxSubArray = function (nums) {
+  let sum = 0;
   let max = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    max_sum_before_ending = Math.max(nums[i], max_sum_before_ending+nums[i]);
-    max = Math.max(max_sum_before_ending, max);
+  for (let num of nums) {
+    sum = Math.max(num, sum + num);
+    max = Math.max(sum, max);
   }
   return max;
 };
 
-let ans = maxSubArray([-2,1,-3,4,-1,2,1,-5,4]);
-// console.log(ans);
-
-var maxSubArray2 = function(nums) {
-  if(!nums.length){
-    return;
-  }
-  let max_sum_before_ending = nums[0];
-  let max = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    if(max_sum_before_ending>0){
-      max_sum_before_ending = max_sum_before_ending + nums[i];
-    }else{
-      max_sum_before_ending = nums[i];
-    }
-    if(max_sum_before_ending > max){
-      max = max_sum_before_ending;
-    }
-  }
-  return max;
-}
-
-let ans2 = maxSubArray2([-2,1,-3,4,-1,2,1,-5,4]);
-console.log(ans2);
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
