af9c813d54e61dfde6e0ede7dcc03faf7bf59884
jamie956 <tgwzzjm@gmail.com>
2020-02-15 16:30:37 +0800

    [Update] 66. Plus One

--- plusOne.js
+++ plusOne.js
@@ -1,29 +1,13 @@
 /*
-Given a non-empty array of digits
-representing a non-negative integer,
-plus one to the integer.
+66. Plus One
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+You may assume the integer does not contain any leading zero, except the number 0 itself.
 
-The digits are stored such that
-the most significant digit is at the head of the list,
-and each element in the array contain a single digit.
+给出一个非空数组，元素组成一个非负整数，给这个整数加1，求加1后的数组
+加1从数组末尾加，如果 当前元素 digits[i]%10==0，说明需要进位，进位后再到下位，看是否需要再进位，如此类推
+如果没有进位，返回结果
 
-You may assume the integer does not contain any leading zero,
-except the number 0 itself.
-
-Input: [1,2,3]
-Output: [1,2,4]
-Explanation: The array represents the integer 123.
-*/
-
-/*
-foreach from end to begin
-  digits[i]++, current digits plus one
-  digits[i] = digits[i]%10, if digits[i]+1=10, digits[i]=0
-  if(digits[i] != 0), => return digits
-case 9999
-digits length + 1
-digits[length]=0
-digit[0]=1
 */
 
 /**
@@ -33,10 +17,8 @@ digit[0]=1
 var plusOne = function(digits) {
   for (let i=digits.length-1; i >= 0; i--) {
     digits[i]++;
-    digits[i] = digits[i]%10;
-    if(digits[i] != 0){
-      return digits;
-    }
+    digits[i] %= 10;
+    if(digits[i] != 0) return digits;
   }
   digits.length++;
   digits[digits.length-1] = 0;
@@ -44,8 +26,4 @@ var plusOne = function(digits) {
   return digits;
 };
 
-let digits;
-// digits = [1,2,3];
-digits = [9,9,9];
-let ans = plusOne(digits);
-console.log(ans);
+console.log(plusOne([9,9,9]));
