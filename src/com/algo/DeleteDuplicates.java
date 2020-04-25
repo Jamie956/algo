8165333ce3e99a1db3105e9130497c28e67d5e3e
Jamie956 <752492509@qq.com>
2020-04-25 09:50:55 +0800

    add DeleteDuplicates

---
+++ src/com/algo/DeleteDuplicates.java
@@ -0,0 +1,51 @@
+package com.algo;
+
+public class DeleteDuplicates {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(2);
+        node1.next = node2;
+        node2.next = node3;
+
+        ListNode ret = deleteDuplicates(node1);
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     * head:
+     * null
+     * 1
+     * 1->2
+     * 1->2->3
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return head;
+
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
