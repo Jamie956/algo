789c90f11dd94c3914e6bffe7e5e1232b2f52c4f
Jamie956 <752492509@qq.com>
2020-04-29 08:42:50 +0800

    update

--- 118.generate.js
+++ 118.generate.js
@@ -3,19 +3,19 @@
 Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
 In Pascal's triangle, each number is the sum of the two numbers directly above it.
 
-给出一个非负整数，生成三角，到numRows行结束
 numRows=0, []
 numRows=1, [1]
 numRows=2, [[1], [1,1]]
 numRows=3, [[1], [1,1], [1,2,1]]
 numRows=4, [[1], [1,1], [1,2,1], [1,3,3,1]]
 
-121
+1210
+0121
 0+1,1+2,2+1,1+0
-根据上一行的左右上元素相加
+
 */
 
-var generate = function (numRows) {
+let generate = function (numRows) {
   if (numRows == 0) return [];
   if (numRows == 1) return [[1]];
   let rows = [[1]];
