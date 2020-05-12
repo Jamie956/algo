ea756e5918038934d6264bebb7bca2e53de5b8e5
Jamie956 <752492509@qq.com>
2020-05-12 10:01:38 +0800

    add hamming weight

--- src/com/algo/Solution.java
+++ src/com/algo/Solution.java
@@ -383,4 +383,19 @@ public class Solution {
         }
         return ret;
     }
+
+    /**
+     * 191. Number of 1 Bits
+     * Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
+     */
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        for(int i=0;i<32;i++){
+            if((n&1)==1) count++;
+            n >>>= 1;
+        }
+
+        return count;
+    }
 }
